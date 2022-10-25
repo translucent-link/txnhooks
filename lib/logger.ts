ee2bb2dd347eed36759b57f797818e774d0ed4ad
Ijonas Kisselbach <ijonas@ijonas.com>
2022-10-25 10:14:07 +0100

    improved logformat

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,6 +1,22 @@
-class Logger {
-  info(message: string | Object) {
-    console.log(message);
-  }
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'txnhooks' },
+});
+
+const logformat = winston.format.printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.label({label: "txnhooks"}),
+      winston.format.timestamp(),
+      logformat
+    ),
+  }));
 }
-export default new Logger();
+export default logger;
